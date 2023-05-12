86fea539525ed686bbe64057323aad55f1a7bf79
joolsoul <ivan.kudinov.2016@mail.ru>
2023-05-12 18:01:08 +0300

    initial commit

---
+++ task4/Matchbox.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace task4;
+
+public class Matchbox
+{
+    private string _manufacturerCompany;
+    private int _numberOfMatches;
+    private double _burnTime;
+
+    public Matchbox(string manufacturerCompany, int numberOfMatches, double burnTime)
+    {
+        _manufacturerCompany = manufacturerCompany;
+        _numberOfMatches = numberOfMatches;
+        _burnTime = burnTime;
+    }
+
+    public string ManufacturerCompany
+    {
+        get => _manufacturerCompany;
+        set => _manufacturerCompany = value;
+    }
+
+    public int NumberOfMatches
+    {
+        get => _numberOfMatches;
+        set => _numberOfMatches = value;
+    }
+
+    public double BurnTime
+    {
+        get => _burnTime;
+        set => _burnTime = value;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder($"Manufacturer company of this matchbox is the {_manufacturerCompany} \n");
+        sb.Append($"Number of matches: {_numberOfMatches}; \n");
+        sb.Append($"Burn time: {_burnTime} s; \n");
+        return sb.ToString();
+    }
+
+    public virtual double GetQ()
+    {
+        return _numberOfMatches * _burnTime;
+    }
+    
+}
---
+++ task4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Коробка спичек: 
+// -	фирма изготовитель; 
+// -	число спичек в коробке; 
+// -	время горения одной спички (с). 
+//     Q = (число спичек) · время 
+
+// P: средний % бракованных 
+// спичек в коробке 
+//  
+//     Qp = ( 100-Р)Q / 100 
+
+
+using task4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Matchbox matchbox = new Matchbox("First Company", 100, 5.5);
+        Console.WriteLine(matchbox.ToString());
+        Console.WriteLine(matchbox.GetQ() + "\n");
+        Console.WriteLine("\n");
+        Console.WriteLine("\n");
+
+        MatchBoxChild matchBoxChild = new MatchBoxChild("Second Company", 110, 6.1, 15);
+        Console.WriteLine(matchBoxChild.ToString());
+        Console.WriteLine(matchBoxChild.GetQ());
+        Console.WriteLine();
+        
+    }
+}
